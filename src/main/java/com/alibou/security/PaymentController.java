dc31d778122bf01240581bc81895701e522f4cfb
Ray9921 <rahulsingh2532c@gmail.com>
2024-05-20 19:31:08 +0530

    2nd commit

---
+++ src/main/java/com/alibou/security/PaymentController.java
@@ -0,0 +1,10 @@
+package com.alibou.security;
+
+public class PaymentController {
+
+    private String paymentType;
+
+    public PaymentController(String paymentType){
+        this.paymentType = paymentType;
+    }
+}
